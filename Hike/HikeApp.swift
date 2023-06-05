bb61d895762b1d85ee1680b19479191ecac2872c
canknbr <cankanbur1@gmail.com>
2023-06-05 16:01:02 +0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Can Kanbur on 5.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
